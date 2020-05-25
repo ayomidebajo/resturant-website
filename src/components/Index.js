d969231e42160c6ec8089b7f87e2cb234b882ab3
https://github.com/ayomidebajo <oluwashinabajo@gmail.com>
2020-05-25 18:08:09 +0100

    Add features component

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './layouts/Navbar'
 import Header from './header/Header'
+import Features from './body/Features'
 
 const Index = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Features />
     </div>
   )
 }
--- src/components/body/Features.js
+++ src/components/body/Features.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Features = () => {
   return (
-    <div>
-      
+    <div className="features--container">
+      <div className="feature--heading">
+        <h4 className="h4--features">Features</h4>
+        <h2 className="h2--features">Why we are the best</h2>
+        <p className="p--features">
+          This minimalist and eclectic composed of several recycled, raw and
+          reworked elements invites from scratch to share a passion for cooking
+        </p>
+      </div>
+      <div className="feature--cards">
+        <div className="card">
+          <h3 className="h3--features">Eat Good Food</h3>
+          <p className="p--features__cards">
+            Let's make something good together up with and get the great tasting
+            need
+          </p>
+        </div>
+        <div className="card">
+          <h3 className="h3--features">Get Solution</h3>
+          <p className="p--features__cards">
+            Let's make something good together up with and get the great tasting
+            need
+          </p>
+        </div>
+        <div className="card">
+          <h3 className="h3--features">Big Savings</h3>
+          <p className="p--features__cards">
+            Let's make something good together up with and get the great tasting
+            need
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Features
+export default Features;
